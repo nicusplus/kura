1d6886451bb57101abf4c126397432e5ddb7fa8b
Maiero <matteo.maiero@eurotech.com>
2019-05-02 11:23:05 +0200

    Fixed versions and tests.
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/test/org.eclipse.kura.core.ssl.test/src/test/java/org/eclipse/kura/core/ssl/SslManagerServiceImplTest.java
+++ kura/test/org.eclipse.kura.core.ssl.test/src/test/java/org/eclipse/kura/core/ssl/SslManagerServiceImplTest.java
@@ -550,6 +550,12 @@ public class SslManagerServiceImplTest {
                     throws NoSuchAlgorithmException, UnsupportedEncodingException {
                 throw new UnsupportedOperationException();
             }
+
+            @Override
+            public String hash(String s, String algorithm)
+                    throws NoSuchAlgorithmException, UnsupportedEncodingException {
+                throw new UnsupportedOperationException();
+            }
         };
 
         result.setKeyStorePassword(KEY_STORE_PATH, "cryptoPassword".toCharArray());
